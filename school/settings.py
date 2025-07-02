150cbcb609d3cd4fe4f57dbabefe748bcf37dca7
mohd kaif <kaifkhan1040@gmail.com>
2025-07-02 12:44:07 +0530

    upda

--- school/settings.py
+++ school/settings.py
@@ -144,7 +144,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets')
 
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'public','static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'public','static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = '/admin/'
