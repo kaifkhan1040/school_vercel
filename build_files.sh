d6c48cc91e7219eb289ef87fd901750c17ad0dbb
mohd kaif <kaifkhan1040@gmail.com>
2025-07-02 14:07:49 +0530

    upda

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,9 @@
-# pip install -r requirements.txt
-# python3.11 manage.py collectstatic
-#!/bin/bash
-echo "Running collectstatic..."
 #!/bin/bash
 echo "Checking Python binaries..."
 which python
 which python3
-which python3.11
 echo "Python version output:"
-python3.11 --version
+python3 --version
 
 echo "Running collectstatic..."
-python3.11 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
