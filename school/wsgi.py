ad1ee2f6ef40ee652fdfa82d155a7751f09b4175
mohd kaif <kaifkhan1040@gmail.com>
2025-07-02 12:12:23 +0530

    upda

--- school/wsgi.py
+++ school/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
+
+app = application
