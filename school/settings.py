edffbc113d43602199f2170c3effe91ed38d8926
mohd kaif <kaifkhan1040@gmail.com>
2025-07-02 13:18:10 +0530

    upda

--- school/settings.py
+++ school/settings.py
@@ -140,13 +140,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'public', 'static'),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets')
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'public','static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build' 'static')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = '/admin/'
