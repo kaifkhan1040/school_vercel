03befb1e2a7d4e16e4fb270b75ae5258ecf39f6c
mohd kaif <kaifkhan1040@gmail.com>
2025-07-02 12:59:10 +0530

    upda

--- school/settings.py
+++ school/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'expenseswebsite', 'static'),
+    os.path.join(BASE_DIR, 'public', 'static'),
 )
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
